2f5f2f014b033e888197775699fafe9ff074c51d
nazmulnahid-git <nazmulnahid.official@gmail.com>
2024-10-01 01:04:00 +0600

    Builing Project Structure

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,6 +30,7 @@ module.exports = {
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
     '@typescript-eslint/no-unused-vars': ['error'],
+    '@typescript-eslint/no-require-imports': 'off',
     'no-console': ['warn', { allow: ['warn', 'error'] }],
     'react-native/no-inline-styles': 'off',
     'react-native/no-unused-styles': 'warn',
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app.{js,jsx,ts,tsx}"], // for more directory "./<directory name>/**/*.{js,jsx,ts,tsx}"
+  content: ['./app.{js,jsx,ts,tsx}'], // for more directory "./<directory name>/**/*.{js,jsx,ts,tsx}"
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};

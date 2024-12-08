d9707497e26cc8232604b66f81961047136b1cf7
nazmulnahid-git <nazmulnahid.official@gmail.com>
2024-12-08 23:36:35 +0600

    working on home

--- app/tabs/index.jsx
+++ app/tabs/index.jsx
@@ -1,15 +1,67 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Pressable } from 'react-native'
 import React from 'react'
 import ScreenWrapper from '@/components/ScreenWrapper'
+import { theme } from '../../constants/theme'
+import { hp, wp } from '@/helpers/common';
+import { IconNotification, IconPlus, IconProfile } from '../../assets/icons/Icons';
+
 
 const HomeScreen = () => {
   return (
-    <ScreenWrapper bg='white'>
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Home</Text>
+    <ScreenWrapper bg="white">
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.title}>ACM Luminary</Text>
+          <View style={styles.icons}>
+            <Pressable>
+              <IconNotification strokeWidth={1.6} height={hp(3.2)} width={hp(3.2)} color={theme.colors.text} />
+            </Pressable>
+            <Pressable>
+              <IconPlus strokeWidth={1.6} height={hp(3.2)} width={hp(3.2)} color={theme.colors.text} />
+            </Pressable>
+            <Pressable>
+              <IconProfile strokeWidth={1.6} height={hp(3.2)} width={hp(3.2)} color={theme.colors.text} />
+            </Pressable>
+          </View>
+        </View>
       </View>
     </ScreenWrapper>
   )
 }
 
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+    marginHorizontal: wp(4),
+  },
+  title: {
+    color: theme.colors.text,
+    fontWeight: theme.fonts.bold,
+    fontSize: hp(3.2),
+
+  },
+  avatarImage: {
+    height: hp(4.3),
+    width: hp(4.3),
+    borderRadius: theme.radius.sm,
+    borderCurve: 'continuous',
+    borderWidth: 3,
+    borderColor: theme.colors.gray,
+
+  },
+  icons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 18,
+  }
+})
--- assets/icons/Icons.jsx
+++ assets/icons/Icons.jsx
@@ -180,6 +180,38 @@ export const IconNotificationOld = ({
   </Svg>
 );
 
+export const IconPlus = ({
+  width,
+  height,
+  color,
+  strokeWidth,
+  ...props
+}) => (
+  <Svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width={width || 24}
+    height={height || 24}
+    color={color || "#000000"}
+    fill="none"
+    {...props}
+  >
+    <Path
+      d="M2.5 12C2.5 7.52166 2.5 5.28249 3.89124 3.89124C5.28249 2.5 7.52166 2.5 12 2.5C16.4783 2.5 18.7175 2.5 20.1088 3.89124C21.5 5.28249 21.5 7.52166 21.5 12C21.5 16.4783 21.5 18.7175 20.1088 20.1088C18.7175 21.5 16.4783 21.5 12 21.5C7.52166 21.5 5.28249 21.5 3.89124 20.1088C2.5 18.7175 2.5 16.4783 2.5 12Z"
+      stroke={color || "currentColor"}
+      strokeWidth={strokeWidth || 1.5}
+      strokeLinejoin="round"
+    />
+    <Path
+      d="M12 8V16M16 12H8"
+      stroke={color || "currentColor"}
+      strokeWidth={strokeWidth || 1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </Svg>
+);
+
 
 export const IconNotification = ({
   width,

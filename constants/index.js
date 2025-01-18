a5ab7dfd0fbb75eaea5cf538b8af56e5077814ae
nazmulnahid-git <nazmulnahid.official@gmail.com>
2025-01-18 23:40:27 +0600

    Update dependencies and enhance image handling
    - Added "expo-image-picker" version 16.0.4 to package.json and package-lock.json for improved image selection capabilities.
    - Updated "expo-image" to version 2.0.4 in package.json for better image handling.
    - Refreshed several dependencies in package-lock.json, including updates to "call-bind", "get-intrinsic", and "gopd" for enhanced performance and security.
    - Introduced new constants for Cloudinary integration, including cloud name and upload preset, to facilitate image uploads.
    - Cleaned up unused imports and ensured consistent formatting across files.

--- constants/index.js
+++ constants/index.js
@@ -1,2 +1,4 @@
 export const supabaseUrl = 'https://hpumpmeiodjgfnukurlb.supabase.co';
 export const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhwdW1wbWVpb2RqZ2ZudWt1cmxiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE2ODQyOTcsImV4cCI6MjA0NzI2MDI5N30.h9WUiLeb4O8S5OuYpCsV2XxTOg93-8aA09cnTOqiNJY'
+export const cloudinaryCloudName = 'drifdisby';
+export const cloudinaryUploadPreset = 'acm-luminary-cld';
